b9ca7b19830d1dcbb98cc715d03a72fe3e992a9f
HakimAzlannn <93250378+HakimAzlannn@users.noreply.github.com>
2021-11-21 01:28:57 +0800

    Update pokemondatabase.py

--- pokemondatabase.py
+++ pokemondatabase.py
@@ -21,14 +21,10 @@ newpkmndata = pkmndata.drop(['#', 'Total','Generation','Legendary','Type1','Type
 Type_1 = ['Grass','Fire','Water','Bug','Normal','Poison','Electric','Ground','Fairy','Fighting','Psychic','Rock','Ice','Dragon','Dark','Ghost','Steel']
 type_1_selection = st.sidebar.multiselect('Type 1', Type_1, Type_1)
 
-## Sidebar - Type 2 selection
-Type_2 = ['Grass','Fire','Water','Bug','Normal','Poison','Electric','Ground','Fairy','Fighting','Psychic','Rock','Ice','Dragon','Dark','Ghost','Steel']
-type_2_selection = st.sidebar.multiselect('Type 2', Type_2, Type_2)
-
 # Sidebar - Legendary selection
 Legendarytype = sorted(pkmndata.Legendary.unique())
 selected_legendary = st.sidebar.multiselect('Legendary', Legendarytype, Legendarytype)
 
-overallpkmndata = newpkmndata[(pkmndata.Legendary.isin(selected_legendary)) & (pkmndata.Type1.isin(type_1_selection)) & (pkmndata.Type2.isin(type_2_selection))]
+overallpkmndata = newpkmndata[(pkmndata.Legendary.isin(selected_legendary)) & (pkmndata.Type1.isin(type_1_selection))]
 st.write('Data Dimension: ' + str(overallpkmndata.shape[0]) + ' rows and ' + str(overallpkmndata.shape[1]) + ' columns.')
 st.dataframe(overallpkmndata)
