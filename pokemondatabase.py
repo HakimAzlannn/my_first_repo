f1089d1e4bf231edd635e6f4909091c78acc91b7
HakimAzlannn <93250378+HakimAzlannn@users.noreply.github.com>
2021-11-22 17:12:52 +0800

    Update pokemondatabase.py

--- pokemondatabase.py
+++ pokemondatabase.py
@@ -2,13 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-[theme]
-primaryColor="#F63366"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="sans serif"
-
 ## Set title
 st.title('Pokemon Database')
 
