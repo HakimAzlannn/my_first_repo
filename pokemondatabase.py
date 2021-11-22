a8f29d13b0862586c0bde7588a6faf647d5ea258
HakimAzlannn <93250378+HakimAzlannn@users.noreply.github.com>
2021-11-22 16:04:12 +0800

    Update pokemondatabase.py

--- pokemondatabase.py
+++ pokemondatabase.py
@@ -9,28 +9,32 @@ This app shows the stats of pokemon!
 * **Pokemon database reference:** [Click here](https://gist.github.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6)
 """)
 
-st.sidebar.header('Pokemon Attribute')
-
 # Upload file
 pkmndata = pd.read_csv('pokemon.csv')
 
 ## Drop column
 newpkmndata = pkmndata.drop(['#', 'Total','Generation','Legendary','Type1','Type2'], axis = 1)
 
-## Sidebar - Type 1 selection
-Type_1 = ['Grass','Fire','Water','Bug','Normal','Poison','Electric','Ground','Fairy','Fighting','Psychic','Rock','Ice','Dragon','Dark','Ghost','Steel']
-type_1_selection = st.sidebar.multiselect('Type', Type_1, Type_1)
+option = st.sidebar.selectbox(
+    'Select your choice',
+     ['Attribute','Status'])
 
-# Sidebar - Legendary selection
-Legendarytype = sorted(pkmndata.Legendary.unique())
-selected_legendary = st.sidebar.multiselect('Legendary', Legendarytype, Legendarytype)
+if option=='Attribute':
+    ## Sidebar - Type 1 selection
+    Type_1 = ['Grass','Fire','Water','Bug','Normal','Poison','Electric','Ground','Fairy','Fighting','Psychic','Rock','Ice','Dragon','Dark','Ghost','Steel']
+    type_1_selection = st.sidebar.multiselect('Type', Type_1, Type_1)
 
-overallpkmndata = newpkmndata[(pkmndata.Legendary.isin(selected_legendary)) & (pkmndata.Type1.isin(type_1_selection))]
-st.write('Data Dimension: ' + str(overallpkmndata.shape[0]) + ' rows and ' + str(overallpkmndata.shape[1]) + ' columns.')
-st.dataframe(overallpkmndata)
+    # Sidebar - Legendary selection
+    Legendarytype = sorted(pkmndata.Legendary.unique())
+    selected_legendary = st.sidebar.multiselect('Legendary', Legendarytype, Legendarytype)
 
-min_speed = int(newpkmndata['Speed'].min())
-max_speed = int(newpkmndata['Speed'].max())
-'# By speed'
-speed = st.slider('Speed', min_speed, max_speed)
-newpkmndata[newpkmndata['Speed'] == speed]
+    overallpkmndata = newpkmndata[(pkmndata.Legendary.isin(selected_legendary)) & (pkmndata.Type1.isin(type_1_selection))]
+    st.write('Data Dimension: ' + str(overallpkmndata.shape[0]) + ' rows and ' + str(overallpkmndata.shape[1]) + ' columns.')
+    st.dataframe(overallpkmndata)
+
+else option=='Status':
+    min_speed = int(newpkmndata['Speed'].min())
+    max_speed = int(newpkmndata['Speed'].max())
+    '# By speed'
+    speed = st.slider('Speed', min_speed, max_speed)
+    newpkmndata[newpkmndata['Speed'] == speed]
