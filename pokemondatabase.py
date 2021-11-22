d8ffcb56bf633278fbf605473c1362756c4edcd2
HakimAzlannn <93250378+HakimAzlannn@users.noreply.github.com>
2021-11-22 14:57:39 +0800

    Update pokemondatabase.py

--- pokemondatabase.py
+++ pokemondatabase.py
@@ -33,3 +33,7 @@ st.markdown("""
 You can use the sidebar button!
 * Also you can search your pokemon here!
 """)
+pkmn_name = st.text_input('pokemon.csv')
+if path:
+    df = pd.read_csv(pkmn_name)
+    df
