a2f452f3418be38073b71ffd0402dc2c6662764e
HakimAzlannn <93250378+HakimAzlannn@users.noreply.github.com>
2021-11-22 16:36:15 +0800

    Update pokemondatabase.py

--- pokemondatabase.py
+++ pokemondatabase.py
@@ -41,7 +41,6 @@ else:
  
     Hp = st.sidebar.slider('HP', min_hp, max_hp)
     speed = st.sidebar.slider('Speed', min_speed, max_speed)
-    data = newpkmndata[(newpkmndata.HP.isin(Hp)) & (newpkmndata.Speed.isin(speed))]
-    ## data = newpkmndata[newpkmndata['HP'] == Hp] & newpkmndata[newpkmndata['Speed'] == speed]
+    data = newpkmndata[newpkmndata['HP'] == Hp] & newpkmndata[newpkmndata['Speed'] == speed]
         
     st.write(data)
